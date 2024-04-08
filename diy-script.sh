f8bda68c44e843ef761883bfa6c5d239faf9b527
goodmen001 <143695131+goodmen001@users.noreply.github.com>
2024-04-08 17:22:21 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -55,7 +55,7 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-a
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 # 新增科学插件
-https://github.com/kiddin9/openwrt-packages/tree/master/luci-app-bypass package/luci-app-bypass
+git clone https://github.com/kiddin9/openwrt-packages/tree/master/luci-app-bypass package/luci-app-bypass
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
